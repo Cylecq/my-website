9a89f98b7bbd49dba2d669f302e2753b3147dba3
Cylecq <leclercq.cyril06@gmail.com>
2023-12-26 19:14:17 +0100

    Add file download functionality to Contact component

--- src/app/[lang]/sections/Contact.tsx
+++ src/app/[lang]/sections/Contact.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import mail from "../../../../public/svg/mail.svg";
@@ -17,6 +18,16 @@ type Props = {
 };
 
 function Contact({ contactDict }: Props) {
+  const handleClick = async () => {
+    const response = await fetch("/api/file");
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "CV_Cyril_Leclercq.pdf";
+    link.click();
+    window.URL.revokeObjectURL(url);
+  };
   return (
     <section id="contact" className="h-screen flex justify-center items-center">
       <div className="bg-white w-11/12 py-4 px-2 z-10 border border-primary shadow-inner flex flex-col justify-center items-center gap-6">
@@ -26,16 +37,21 @@ function Contact({ contactDict }: Props) {
         <div className="flex flex-col gap-10">
           <div className="w-full flex flex-col items-center gap-2">
             <h3>{contactDict.mailTitle}</h3>
-            <button className="relative h-[50px] w-56 overflow-hidden border border-primary bg-white text-primary shadow-2xl transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full before:bg-primary before:duration-500 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-0 after:w-full after:bg-primary after:duration-500 hover:text-black hover:shadow-primary hover:before:h-2/4 hover:after:h-2/4 hover:border-black">
-              <span className="relative z-10 flex items-center justify-center gap-4">
-                {contactDict.mailButton}
-                <Image src={mail} alt="mail" width={30} height={30} />
-              </span>
-            </button>
+            <a href="mailto:leclercq.cyril06@gmail.com">
+              <button className="relative h-[50px] w-56 overflow-hidden border border-primary bg-white text-primary shadow-2xl transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full before:bg-primary before:duration-500 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-0 after:w-full after:bg-primary after:duration-500 hover:text-black hover:shadow-primary hover:before:h-2/4 hover:after:h-2/4 hover:border-black">
+                <span className="relative z-10 flex items-center justify-center gap-4">
+                  {contactDict.mailButton}
+                  <Image src={mail} alt="mail" width={30} height={30} />
+                </span>
+              </button>
+            </a>
           </div>
           <div className="w-full flex flex-col items-center gap-2">
             <h3>{contactDict.cvTitle}</h3>
-            <button className="relative h-[50px] w-56 overflow-hidden border border-primary bg-white text-primary shadow-2xl transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full before:bg-primary before:duration-500 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-0 after:w-full after:bg-primary after:duration-500 hover:text-black hover:shadow-primary hover:before:h-2/4 hover:after:h-2/4 hover:border-black">
+            <button
+              onClick={handleClick}
+              className="relative h-[50px] w-56 overflow-hidden border border-primary bg-white text-primary shadow-2xl transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full before:bg-primary before:duration-500 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-0 after:w-full after:bg-primary after:duration-500 hover:text-black hover:shadow-primary hover:before:h-2/4 hover:after:h-2/4 hover:border-black"
+            >
               <span className="relative z-10 flex items-center justify-center gap-4">
                 {contactDict.cvButton}
                 <Image src={download} alt="download" width={30} height={30} />
---
+++ src/app/api/file/route.ts
@@ -0,0 +1,19 @@
+import { readFile } from "fs/promises";
+import path from "path";
+
+export async function GET() {
+  const buffer = await readFile(
+    path.join(process.cwd(), "src/app/download", "CV_Cyril_Leclercq.pdf")
+  );
+
+  const headers = new Headers();
+  headers.append(
+    "Content-Disposition",
+    'attachment; filename="CV_Cyril_Leclercq.pdf"'
+  );
+  headers.append("Content-Type", "image/png");
+
+  return new Response(buffer, {
+    headers,
+  });
+}
