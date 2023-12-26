39dc4b5bb58048f6f93438b9935451d30b290051
Cylecq <leclercq.cyril06@gmail.com>
2023-12-26 19:39:48 +0100

    Add DownloadCVButton component and refactor Contact section

---
+++ src/app/[lang]/components/DownloadCVButton.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Image from "next/image";
+
+type Props = {
+  buttonText: string;
+  icon: any;
+};
+
+const DownloadCVButton = ({ buttonText, icon }: Props) => {
+  const handleDownload = async () => {
+    try {
+      const response = await fetch("/api/file");
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "CV_Cyril_Leclercq.pdf";
+      link.click();
+
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading CV:", error);
+    }
+  };
+
+  return (
+    <button
+      onClick={handleDownload}
+      className="relative h-[50px] w-56 overflow-hidden border border-primary bg-white text-primary shadow-2xl transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full before:bg-primary before:duration-500 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-0 after:w-full after:bg-primary after:duration-500 hover:text-black hover:shadow-primary hover:before:h-2/4 hover:after:h-2/4 hover:border-black"
+    >
+      <span className="relative z-10 flex items-center justify-center gap-4">
+        {buttonText}
+        <Image src={icon} alt={buttonText} width={30} height={30} />
+      </span>
+    </button>
+  );
+};
+
+export default DownloadCVButton;
--- src/app/[lang]/sections/Contact.tsx
+++ src/app/[lang]/sections/Contact.tsx
@@ -1,6 +1,6 @@
-"use client";
 import Image from "next/image";
 import React from "react";
+import DownloadCVButton from "../components/DownloadCVButton";
 import mail from "../../../../public/svg/mail.svg";
 import download from "../../../../public/svg/download.svg";
 import github from "../../../../public/svg/github.svg";
@@ -18,16 +18,12 @@ type Props = {
 };
 
 function Contact({ contactDict }: Props) {
-  const handleClick = async () => {
-    const response = await fetch("/api/file");
-    const blob = await response.blob();
-    const url = window.URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = "CV_Cyril_Leclercq.pdf";
-    link.click();
-    window.URL.revokeObjectURL(url);
-  };
+  const createContactLink = (url: string, text: string, icon: string) => (
+    <a href={url} target="_blank" className="shadow-2xl">
+      <Image src={icon} alt={text} width={30} height={30} />
+    </a>
+  );
+
   return (
     <section id="contact" className="h-screen flex justify-center items-center">
       <div className="bg-white w-11/12 py-4 px-2 z-10 border border-primary shadow-inner flex flex-col justify-center items-center gap-6">
@@ -48,33 +44,24 @@ function Contact({ contactDict }: Props) {
           </div>
           <div className="w-full flex flex-col items-center gap-2">
             <h3>{contactDict.cvTitle}</h3>
-            <button
-              onClick={handleClick}
-              className="relative h-[50px] w-56 overflow-hidden border border-primary bg-white text-primary shadow-2xl transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full before:bg-primary before:duration-500 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-0 after:w-full after:bg-primary after:duration-500 hover:text-black hover:shadow-primary hover:before:h-2/4 hover:after:h-2/4 hover:border-black"
-            >
-              <span className="relative z-10 flex items-center justify-center gap-4">
-                {contactDict.cvButton}
-                <Image src={download} alt="download" width={30} height={30} />
-              </span>
-            </button>
+            <DownloadCVButton
+              buttonText={contactDict.cvButton}
+              icon={download}
+            />
           </div>
           <div className="w-full flex flex-col items-center gap-2">
             <h3>{contactDict.networksTitle}</h3>
             <div className="w-full flex justify-around">
-              <a
-                target="blank"
-                className="shadow-2xl"
-                href="https://github.com/Cylecq"
-              >
-                <Image src={github} alt="github" width={30} height={30} />
-              </a>
-              <a
-                target="blank"
-                className="shadow-2xl"
-                href="https://www.linkedin.com/in/cylecq/"
-              >
-                <Image src={linkedin} alt="linkedin" width={30} height={30} />
-              </a>
+              {createContactLink(
+                "https://github.com/Cylecq",
+                "github link",
+                github
+              )}
+              {createContactLink(
+                "https://www.linkedin.com/in/cylecq/",
+                "linkedin link",
+                linkedin
+              )}
             </div>
           </div>
         </div>
