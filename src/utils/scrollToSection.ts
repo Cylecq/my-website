7c2e74288d2156315e288b330b5f9da7fa6ea729
Cylecq <leclercq.cyril06@gmail.com>
2026-05-22 22:46:28 +0200

    chore: v2 reset

--- src/utils/scrollToSection.ts
+++
@@ -1,6 +0,0 @@
-export default function scrollToSection(sectionId: string) {
-  const section = document.querySelector(sectionId);
-  if (section) {
-    section.scrollIntoView({ behavior: "smooth" });
-  }
-}
